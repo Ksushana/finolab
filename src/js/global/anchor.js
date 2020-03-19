4a2e8e1c59bd51c5a14b680fd60786bc77db0275
Ksenia Balashova <utyatka@gmail.com>
2020-03-19 16:06:19 +0100

    fix

---
+++ src/js/global/anchor.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#links").on("click", "a.header__link--phone", function(event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top + $(id).height() / 2 - $(window).height() / 2;
+    setTimeout(() => $("body,html").animate({ scrollTop: top }, 1000), 500);
+  });
+});
