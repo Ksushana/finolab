058c214fb4f86016aa10a3a29acdad14e73e4232
Ksenia Balashova <utyatka@gmail.com>
2020-03-21 17:37:11 +0100

    sliders

--- build/js/script.js
+++ build/js/script.js
@@ -10526,62 +10526,114 @@ $(function() {
   const projectsSlider = document.querySelector(
     "section.projects .projects__slider .swiper-container"
   );
+  if (!projectsSlider) {
+    return;
+  }
 
-  if (projectsSlider) {
-    const projectsSwiperSlider = new window.Swiper(projectsSlider, {
-      loop: true,
-      breakpointsInverse: true,
-      slidesPerView: "auto",
-      spaceBetween: 17,
-      centeredSlides: true,
-      pagination: {
-        el: ".swiper-pagination--pr",
-        type: "bullets",
-        clickable: true
+  const mustUseSlider = () => {
+    const slides = projectsSlider.querySelectorAll(".swiper-slide");
+    if (window.innerWidth >= 970 && slides.length <= 3) {
+      return false;
+    }
+    return true;
+  };
+
+  if (!mustUseSlider()) {
+    const arrows = document.querySelectorAll(
+      "section.projects .projects__slider-arrow"
+    );
+    const container = document.querySelector(
+      ".projects__slider .swiper-wrapper"
+    );
+    container.classList.add("space-around");
+    arrows.forEach(arrow => arrow.classList.add("visually-hidden"));
+    return;
+  }
+
+  const projectsSwiperSlider = new window.Swiper(projectsSlider, {
+    loop: false,
+    breakpointsInverse: true,
+    slidesPerView: "auto",
+    spaceBetween: 17,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination--pr",
+      type: "bullets",
+      clickable: true
+    },
+    breakpoints: {
+      970: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        centeredSlides: false
       },
-      breakpoints: {
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 32
-        },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 32,
-          navigation: {
-            nextEl: "section.projects .projects__slider-arrow--next",
-            prevEl: "section.projects .projects__slider-arrow--prev"
-          }
+      1280: {
+        slidesPerView: 3,
+        centeredSlides: false,
+        spaceBetween: 32,
+        navigation: {
+          nextEl: "section.projects .projects__slider-arrow--next",
+          prevEl: "section.projects .projects__slider-arrow--prev"
         }
       }
-    });
-  }
+    }
+  });
 })();
 
 (function() {
-  let mediaSlider = document.querySelector(".media__slider .swiper-container");
+  const mediaSlider = document.querySelector(
+    ".media__slider .swiper-container"
+  );
+  if (!mediaSlider) {
+    return;
+  }
 
-  if (mediaSlider) {
-    let mediaSwiperSlider = new Swiper(mediaSlider, {
-      loop: true,
-      breakpointsInverse: true,
-      slidesPerView: "auto",
-      spaceBetween: 16,
-      centeredSlides: true,
-      pagination: {
-        el: ".swiper-pagination--media",
-        type: "bullets",
-        clickable: true
+  const mustUseSlider = () => {
+    const slides = mediaSlider.querySelectorAll(".swiper-slide");
+    if (window.innerWidth >= 1000 && slides.length <= 3) {
+      return false;
+    }
+    return true;
+  };
+
+  if (!mustUseSlider()) {
+    const arrows = document.querySelectorAll(".media__slider-arrow");
+    arrows.forEach(arrow => arrow.classList.add("visually-hidden"));
+    const container = document.querySelector(".media__slider .swiper-wrapper");
+    container.classList.add("space-around");
+    const pag = document.querySelector(".media__slider .swiper-pagination");
+    pag.classList.add("visually-hidden");
+    const slides = document.querySelectorAll(".media__slider .swiper-slide");
+    slides.forEach(slide => slide.classList.add("adaptive"));
+    return;
+  }
+
+  const mediaSwiperSlider = new window.Swiper(mediaSlider, {
+    loop: false,
+    breakpointsInverse: true,
+    slidesPerView: "auto",
+    centeredSlidesBounds: true,
+    spaceBetween: 16,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination--media",
+      type: "bullets",
+      clickable: true
+    },
+    breakpoints: {
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        centeredSlides: false
       },
-      breakpoints: {
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 32,
-          navigation: {
-            nextEl: ".media__slider-arrow--next",
-            prevEl: ".media__slider-arrow--prev"
-          }
+      1280: {
+        slidesPerView: 3,
+        centeredSlides: false,
+        navigation: {
+          nextEl: ".media__slider-arrow--next",
+          prevEl: ".media__slider-arrow--prev"
         }
       }
-    });
-  }
+    }
+  });
 })();
